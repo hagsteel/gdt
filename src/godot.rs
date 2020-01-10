0106d65d0a8e723b4c6df1566c53b6f212840fe1
Jonas Hagstedt <hagstedt@gmail.com>
2020-01-10 11:57:24 +0100

    Default window size to 1280x720

--- src/godot.rs
+++ src/godot.rs
@@ -4,10 +4,16 @@ use std::fs::File;
 const PROJECT_TEMPLATE_NAME: &'static str = "project.godot";
 const TEMPLATE: &'static str = r#"[application]
 
-config/name="#;
+config/name={name}
+
+[display]
+
+window/size/width=1280
+window/size/height=720
+"#;
 
 pub fn init(name: String) {
-    let template = format!("{}\"{}\"", TEMPLATE, name);
+    let template = TEMPLATE.replace("{name}", &name);
     let mut file = match File::create(PROJECT_TEMPLATE_NAME) {
         Ok(file) => file,
         Err(e) => {
